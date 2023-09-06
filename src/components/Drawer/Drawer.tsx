67d103391ab68d558443469ed93580161f05046a
scarlet <scarryaa@gmail.com>
2023-09-06 12:58:21 -0400

    Fixed player overlapping lyrics/queue

--- src/components/Drawer/Drawer.tsx
+++ src/components/Drawer/Drawer.tsx
@@ -55,6 +55,15 @@ const handleLyricsClick = () => {
       : "18rem",
   );
 
+  document.body.style.setProperty(
+    "--side-panel-offset",
+    rightPanelOpen.value
+      ? rightPanelContent.value === "queue"
+        ? "14rem"
+        : "0rem"
+      : "14rem",
+  );
+
   (rightPanelOpen.value === true && rightPanelContent.value === "lyrics")
     ? setRightPanelOpen({ value: false })
     : setRightPanelOpen({ value: true });
@@ -62,7 +71,7 @@ const handleLyricsClick = () => {
     (rightPanelOpen.value === true && rightPanelContent.value !== "lyrics")
     ? setRightPanelContent({ value: "lyrics" })
     : setRightPanelContent({ value: "" });
-    
+
   if (rightPanelOpen.value) {
     fetchLyrics();
   }
