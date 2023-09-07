7c7fbce808ee4db82d86c127a918460d94e9e7c0
Alan Miles <thesorton@icloud.com>
2023-09-07 08:15:01 -0700

    handle the click

--- src/pages/Library/Artists/Artists.tsx
+++ src/pages/Library/Artists/Artists.tsx
@@ -1,6 +1,6 @@
-import { Switch, Match, For } from "solid-js";
+import { Switch, Match, createSignal, For } from "solid-js";
 import { LoadingSpinner } from "../../../components/LoadingSpinner/LoadingSpinner";
-import { getNestedRelationships, getItemAttributes, replaceSrc, getItemRelationships } from "../../../util/utils";
+import { getNestedRelationships, replaceSrc, getItemRelationships } from "../../../util/utils";
 import styles from "./Artists.module.scss";
 import { ArtistTile } from "../../../components/ArtistTile/ArtistTile";
 import { createLibraryArtistsStore } from "../../../stores/api-store";
@@ -11,9 +11,17 @@ export function Artists() {
   const artistsStore = createLibraryArtistsStore();
   const artistsData = artistsStore();
 
+  // State to hold the selected artist
+  const [selectedArtist, setSelectedArtist] = createSignal(null);
+
+  const handleArtistClick = (artist) => {
+    setSelectedArtist(artist);
+    console.log(selectedArtist().attributes.name)
+  };
+  
   return (
     <>
-      <div class={styles.artists}>
+      <div class={styles.artists__list}>
         <Switch fallback={<div>Not found</div>}>
           <Match
             when={
@@ -28,33 +36,31 @@ export function Artists() {
             <Error error={artistsData.error} />
           </Match>
           <Match when={artistsData.state === "ready"}>
-          <div class={styles.artists__list}>
             <For each={artistsData()?.data}>
               {(artist) => (
-                console.log(artist),
-                  <ArtistTableTile
-                    id={getItemRelationships(artist)?.catalog?.data?.[0]?.id}
-                    type={artist.type}
-                    title={artist.attributes.name}
-                    artists={getNestedRelationships(artist)?.artists?.data?.map(
-                      (artist: any) => artist.attributes.name,
-                    )}
-                    mediaArt={
-                      getItemRelationships(artist)?.catalog?.data?.[0]
-                        ?.attributes?.artwork && {
-                        url: replaceSrc(
-                          getItemRelationships(artist)?.catalog?.data?.[0]
-                            ?.attributes?.artwork.url,
-                          300,
-                        ),
-                      } || {
-                        url: "",
-                      }
-                    }
-                  />
+                <ArtistTableTile
+                  id={getItemRelationships(artist)?.catalog?.data?.[0]?.id}
+                  type={artist.type}
+                  title={artist.attributes.name}
+                  artists={getNestedRelationships(artist)?.artists?.data?.map(
+                    (artist: any) => artist.attributes.name
                   )}
+                  mediaArt={
+                    getItemRelationships(artist)?.catalog?.data?.[0]
+                      ?.attributes?.artwork && {
+                      url: replaceSrc(
+                        getItemRelationships(artist)?.catalog?.data?.[0]
+                          ?.attributes?.artwork.url,
+                        300,
+                      ),
+                    } || {
+                      url: "",
+                    }
+                  }
+                  onClick={() => handleArtistClick(artist)}
+                />
+              )}
             </For>
-          </div>
           </Match>
         </Switch>
       </div>
