eabbb398de19dce47e182f538df40931478c732b
Alan Miles <thesorton@icloud.com>
2023-09-08 11:30:51 -0700

    change how we get lib albums

--- src/pages/Library/Artists/Artists.tsx
+++ src/pages/Library/Artists/Artists.tsx
@@ -1,6 +1,5 @@
 import {
   Switch,
-  Show,
   Match,
   createSignal,
   For
@@ -9,40 +8,37 @@ import { MediaTile } from "../../../components/MediaTile/MediaTile";
 import { LoadingSpinner } from "../../../components/LoadingSpinner/LoadingSpinner";
 import { getItemAttributes, getItemRelationships, getNestedRelationships, replaceSrc } from "../../../util/utils";
 import styles from "./Artists.module.scss";
-import { createLibraryAlbumsStore, createLibraryArtistsStore } from "../../../stores/api-store";
+import { createLibraryArtistsStore } from "../../../stores/api-store";
 import { Error } from "../../../components/Error/Error";
 import { ArtistTableTile } from "../../../components/ArtistTableTile/ArtistTableTile";
+import { fetchLibraryAlbum } from "../../../api/get-artist-album";
 
 export function Artists() {
 
   const artistsStore = createLibraryArtistsStore();
   const artistsData = artistsStore();
 
-  const albumStore = createLibraryAlbumsStore();
-  const albumData = albumStore();
+  let albums: any;
 
   // State to hold the selected artist
   const [selectedArtist, setSelectedArtist] = createSignal(null);
+  const [recievedAlbums, setRecievedAlbums] = createSignal(null);
+  const [albumArray, setAlbumArray] = createSignal([]);
+
 
   const handleArtistClick = async (artist) => {
     setSelectedArtist(artist);
     console.log(selectedArtist());
-    const data = albumData();
-    if (data) {
-      console.log(data)
-      const results = findAlbumsByArtist(artist.attributes.name.toString(), data);
-      console.log(results); // log the results
-    }
-  }
 
-  function findAlbumsByArtist(artistName: string, jsonData: any): [string, string][] {
-    // Find the top-level data objects where the name attribute matches the search string
-    let result: [string, string][] = [];
-    console.log(jsonData.data)
-    if (jsonData.data && jsonData.data.length) {
-      result = jsonData.data.filter((album: any) => album.attributes.artistName.toLowerCase() === artistName.toLowerCase());
-    }
-    return result;
+    setRecievedAlbums(await fetchLibraryAlbum({
+      devToken: import.meta.env.VITE_MUSICKIT_TOKEN,
+      musicUserToken: MusicKit.getInstance()?.musicUserToken,
+      id: artist.id,
+    }))
+    albums = Object.keys(recievedAlbums().resources["library-albums"]).map((key, index) => {
+      return { ...recievedAlbums().resources["library-albums"][key], index: index };
+    });
+    setAlbumArray(albums)
   }
 
   return (
@@ -92,49 +88,32 @@ export function Artists() {
         {/* Second column */}
         {selectedArtist() && (
           <div class={styles.artists__albums}>
-            {/* Render content for the second column here */}
             <h2>{selectedArtist().attributes.name}</h2>
-            <Switch fallback={<div>Not found</div>}>
-              <Match when={
-                albumData.state === "pending" ||
-                albumData.state === "unresolved" ||
-                albumData.state === "refreshing"
-              }
-            >
-                <LoadingSpinner />
-              </Match>
-              <Match when={albumData.state === "errored"}>
-                <Error error={albumData.error} />
-              </Match>
-              <Match when={albumData.state === "ready"}>
-                {/* Use the result of the findAlbumsByArtist function to render the albums */}
-                <div class={styles.albums}>
-                  <For each={findAlbumsByArtist(selectedArtist().attributes.name, albumData())}>
-                    {(album) => (
-                      <MediaTile
-                        id={album.id}
-                        type={album.type}
-                        title={album.attributes.name}
-                        artists={
-                          getNestedRelationships(album)?.artists?.data?.map(
-                            (artist: any) => artist.attributes.name,)
-                        }
-                        artistIds={
-                          getNestedRelationships(album)?.artists?.data?.map(
-                            (artist: any) => artist.id,)
-                        }
-                        mediaArt={getItemAttributes(album).artwork && {
-                          url: replaceSrc(getItemAttributes(album).artwork.url, 300)
-                        }}
-                      />
+            <div class={styles.albums}>
+              <For each={albumArray()}>
+                {(album) => (
+                  <MediaTile
+                    id={album.id}
+                    type={album.type}
+                    title={album.attributes.name}
+                    artists={getNestedRelationships(album)?.artists?.data?.map(
+                      (artist: any) => artist.attributes.name,
                     )}
-                  </For>
-                </div>
-              </Match>
-            </Switch>
+                    artistIds={getNestedRelationships(album)?.artists?.data?.map(
+                      (artist: any) => artist.id,
+                    )}
+                    mediaArt={
+                      getItemAttributes(album).artwork && {
+                        url: replaceSrc(getItemAttributes(album).artwork.url, 300),
+                      }
+                    }
+                  />
+                )}
+              </For>
+            </div>
           </div>
         )}
       </div>
-    </>
-  );
+      </>
+    );
 }
