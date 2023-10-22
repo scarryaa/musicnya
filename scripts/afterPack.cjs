6001d9d1ce226b8632f61662260aa4330a525c91
scarlet <scarryaa@gmail.com>
2023-10-22 13:09:31 -0400

    Updated EVS signing code

--- scripts/afterPack.cjs
+++ scripts/afterPack.cjs
@@ -6,11 +6,19 @@ exports.default = function (context) {
   const { execSync } = require("child_process");
   console.log("VMP signing start");
 
+  // Load EVS credentials from environment variables
+  const username = process.env.EVS_USERNAME;
+  const password = process.env.EVS_PASSWORD;
+
+  if (!username || !password) {
+    console.log("EVS credentials are missing");
+    return;
+  }
+
   const command =
     process.platform === "darwin"
-      ? "python3 -m castlabs_evs.vmp sign-pkg ./dist/mac " + context.appOutDir
-      : "python -m castlabs_evs.vmp sign-pkg ./dist/win-unpacked " +
-        context.appOutDir;
+      ? `EVS_USERNAME="${username}" EVS_PASSWORD="${password}" python3 -m castlabs_evs.vmp sign-pkg ./dist/mac ${context.appOutDir}`
+      : `EVS_USERNAME="${username}" EVS_PASSWORD="${password}" python -m castlabs_evs.vmp sign-pkg ./dist/win-unpacked ${context.appOutDir}`;
 
   execSync(command);
   console.log("VMP signing complete");
